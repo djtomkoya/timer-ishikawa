49ddf723b59ca67fb6c50edf8fc9402bef787820
djtomkoya <djtomkoya@gmail.com>
2025-07-07 12:35:02 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyAfWHKmXad82HBw0zU-8k1t5OGKP5zhAdA",
+  authDomain: "timer-ishikawa.firebaseapp.com",
+  databaseURL: "https://timer-ishikawa-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "timer-ishikawa",
+  appId: "1:367450670827:web:a721a9f1484370ca8e20ae",
+  measurementId: "G-E7G8PKG9BZ",
+  messagingSenderId: "367450670827"
+};
